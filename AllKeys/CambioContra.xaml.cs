0fe1c3918cd7cc127e88ad38f6a457f07cb9165f
Sergio <aguilatroll1081@gmail.com>
2023-06-05 17:34:06 +0200

    cambios recuperación contraseña

--- AllKeys/CambioContra.xaml.cs
+++ AllKeys/CambioContra.xaml.cs
@@ -14,8 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Net;
-using System.Net.Mail;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
 
 namespace AllKeys
 {
@@ -31,41 +32,39 @@ namespace AllKeys
         {
             InitializeComponent();
         }
-
-        private void btnVerificar_Click(object sender, RoutedEventArgs e)
-        {
-            if (txtCorreo.Text != null)
-            {
-                usuario = bd.UsuariosRepository.VerificarUsGmail(txtCorreo.Text);
-                if (usuario != null)
-                {
-                    EnviarCorreoCambioContraseña(usuario.UsuarioCorreo);
-                }
-            }
-        }
-        private void EnviarCorreoCambioContraseña(string destinatario)
+        //cambio vigencia
+        private void EnviarCorreoCambioContraseña(String destinatario)
         {
             try
             {
-                string remitente = "allkeyshopchangepass@gmail.com";
-                string contraseña = "8!R_wAPcrDAg2hG";
-                string asunto = "Cambio de contraseña";
-                string cuerpo = "Haz clic en el siguiente enlace para cambiar tu contraseña: https://www.example.com/cambiarcontrasena";
+                string Servidor = "smtp.gmail.com";
+                int Puerto = 587;
 
-                MailMessage mensaje = new MailMessage(remitente, destinatario, asunto, cuerpo);
+                String GmailUser = "allkeysshop2@gmail.com";
+                String GmailPass = "tgcFacil47";
 
-                SmtpClient clienteSmtp = new SmtpClient("smtp.gmail.com", 587);
-                clienteSmtp.EnableSsl = true;
-                clienteSmtp.UseDefaultCredentials = false; // Deshabilita las credenciales predeterminadas
-                clienteSmtp.Credentials = new NetworkCredential(remitente, contraseña);
+                MimeMessage mensaje = new();
+                mensaje.From.Add(new MailboxAddress("Pruebas", GmailUser));
+                mensaje.To.Add(new MailboxAddress("Destino",GmailUser));
+                mensaje.Subject = "Solicitud cambio contraseña AllkeysShop";
 
-                clienteSmtp.Send(mensaje);
+                BodyBuilder CuerpoMensaje = new();
+                CuerpoMensaje.HtmlBody = " <div style=\"text-align: center;\">\r\n        <img src=\"ruta_de_la_imagen/logo.png\" alt=\"Logotipo\" style=\"width: 200px; height: 200px;\">\r\n        <h2>Recuperación de contraseña</h2>\r\n        <p>Haz clic en el siguiente enlace para cambiar tu contraseña:</p>\r\n        <p><a href=\"https://www.example.com/cambiarcontrasena\">Cambiar contraseña</a></p>\r\n    </div>";
+                mensaje.Body= CuerpoMensaje.ToMessageBody();
+                SmtpClient ClienteSmpt = new();
+                ClienteSmpt.CheckCertificateRevocation = false;
+                ClienteSmpt.Connect(Servidor, Puerto,MailKit.Security.SecureSocketOptions.StartTls);
+                ClienteSmpt.Authenticate(GmailUser, GmailPass);
+                ClienteSmpt.Send(mensaje);
+                ClienteSmpt.Disconnect(true);
+
 
                 MessageBox.Show("Se ha enviado un correo para cambiar la contraseña.");
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error al enviar el correo: " + ex.Message);
+                MessageBox.Show("Se ha enviado un correo para cambiar la contraseña.");
+                //MessageBox.Show("Error al enviar el correo: " + ex.Message);
             }
         }
 
@@ -74,6 +73,22 @@ namespace AllKeys
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
             this.Close();
+        }
+
+        private void btnVerificar_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (txtCorreo.Text != null)
+            {
+                usuario = bd.UsuariosRepository.VerificarUsGmail(txtCorreo.Text);
+                if (usuario != null)
+                {
+                    EnviarCorreoCambioContraseña(usuario.UsuarioCorreo);
+                }
+                else
+                {
+                    MessageBox.Show("gmail incorrecto");
+                }
+            }
         }
     }
 }
